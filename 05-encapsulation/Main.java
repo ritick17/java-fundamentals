1f468279a2fd1b94f12bd7b823436577bbeadd9b
Ritick Choudhary <ritickchoudhary7@gmail.com>
2026-09-14 22:54:04 +0530

    feat: add encapsulation example

---
+++ 05-encapsulation/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Friends dost=new Friends();
+        dost.setName("Java");
+        dost.setAge(15);
+        dost.setProfession("Graduation");
+
+        System.out.println(dost);
+
+    }
+}
